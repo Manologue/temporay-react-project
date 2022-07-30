550ef9afaea6ab48bad4c58ad33e4deb12638763
Moumi Manoah <manomoumi@gmail.com>
2022-07-30 09:45:40 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, nostrum
+        quidem! Ad sint molestiae tempora accusamus hic, reprehenderit impedit
+        quas?
+      </p>
+    </section>
+  );
+};
+
+export default About;
